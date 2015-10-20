341d108df81c0f06afcd12151920e9ee5ad44082
hushwolf <austin@ideum.com>
2015-10-20 13:08:09 -0600

    removed functions for #inquiry and #sales

--- js/main.js
+++ js/main.js
@@ -341,15 +341,6 @@ $(document).ready(function($) {
 
     });
 
-  // function for email and spec sheet link on contact page
-  $('#inquiry').click(function() {
-    window.open('http://ideum.com/contact');
-  });
-
-  $('#sales').click(function() {
-    window.location.href = 'mailto:sales@ideum.com';
-  });
-
   $win.on("scroll", function () { // on scroll do this
     //console.log($(this).scrollTop());
 
